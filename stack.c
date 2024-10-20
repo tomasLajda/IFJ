4b82a115703fd5b1b268930766cf9dadd3c53d07
Matúš Csirik <matus.csirik@gmail.com>
2024-10-20 23:05:39 +0200

    stack.c first draft

--- stack.c
+++ stack.c
@@ -7,23 +7,70 @@
  */
 
 #include "stack.h"
+#include "error_codes.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-void initStack(Stack *stack) {}
+void initStack(Stack *stack) { stack->top = NULL; }
 
-void initStackElement(StackElement *element, Token *tokenPtr) {}
+void initStackElement(StackElement *element, Token *tokenPtr) {
+    element->tokenPtr = tokenPtr;
+    element->next = NULL;
+}
 
-bool isEmpty(Stack *stack) {}
+bool isEmpty(Stack *stack) { return stack->top == NULL; }
 
-void push(Stack *stack, StackElement *elementPtr) {}
+void push(Stack *stack, StackElement *elementPtr) {
+    if (isEmpty(stack)) {
+        stack->top = elementPtr;
+    } else {
+        elementPtr->next = stack->top;
+        stack->top = elementPtr;
+    }
+}
 
-void pop(Stack *stack) {}
+void pop(Stack *stack) {
+    if (isEmpty(stack)) {
+        fprintf(stderr, "Stack is empty\n");
+        exit(INTERNAL_ERROR);
+    }
+    StackElement *tmp = stack->top;
+    stack->top = stack->top->next;
+    free(tmp);
+}
 
-StackElement *top(Stack *stack) {}
+StackElement *top(Stack *stack) {
+    if (isEmpty(stack)) {
+        fprintf(stderr, "Stack is empty\n");
+        exit(INTERNAL_ERROR);
+    }
+    return stack->top;
+}
 
-Token *topToken(Stack *stack) {}
+Token *topToken(Stack *stack) {
+    if (isEmpty(stack)) {
+        fprintf(stderr, "Stack is empty\n");
+        exit(INTERNAL_ERROR);
+    }
+    return stack->top->tokenPtr;
+}
 
-void display(Stack *stack) {}
+void display(Stack *stack) {
+    if (isEmpty(stack)) {
+        printf("Stack is empty\n");
+        return;
+    }
+    printf("Top -> ");
+    StackElement *current = stack->top;
+    while (current != NULL) {
+        printf("| %d ", current->tokenPtr->type);
+        current = current->next;
+    }
+    printf("|\n");
+}
 
-void cleanupStack(Stack *stack) {}
+void cleanupStack(Stack *stack) {
+    while (!isEmpty(stack)) {
+        pop(stack);
+    }
+}
