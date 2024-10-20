f51e1d7de8b17273957549cfa28e9a86961c90f4
Matúš Csirik <matus.csirik@gmail.com>
2024-10-20 22:57:18 +0200

    implemented stack.c skeleton

--- stack.c
+++ stack.c
@@ -5,3 +5,25 @@
  * @author Matúš Csirik - xcsirim00
  *
  */
+
+#include "stack.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void initStack(Stack *stack) {}
+
+void initStackElement(StackElement *element, Token *tokenPtr) {}
+
+bool isEmpty(Stack *stack) {}
+
+void push(Stack *stack, StackElement *elementPtr) {}
+
+void pop(Stack *stack) {}
+
+StackElement *top(Stack *stack) {}
+
+Token *topToken(Stack *stack) {}
+
+void display(Stack *stack) {}
+
+void cleanupStack(Stack *stack) {}
