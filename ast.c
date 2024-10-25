ecdf3f988b65a2318f6546d5be854a2b5a6f04c2
Martin Valapka <martin.valapka1@gmail.com>
2024-10-25 21:33:12 +0200

    Creation of ast.c file

---
+++ ast.c
@@ -0,0 +1,17 @@
+/*
+ * IFJ Project
+ * @brief Implementation file of AST
+ *
+ * @author Martin Valapka - xvalapm00
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include "ast.h"
+
+int main(int argc, char const *argv[]) {
+    return 0;
+}
