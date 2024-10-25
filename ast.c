3319d789f7d2dbb3475547cd3885301721276702
Martin Valapka <martin.valapka1@gmail.com>
2024-10-25 23:10:25 +0200

    Added creation function of AST

--- ast.c
+++ ast.c
@@ -11,6 +11,16 @@
 #include <string.h>
 #include <stdbool.h>
 #include "ast.h"
+#include "error_codes.h"
+
+AST* createAST() {
+    AST* tree = (AST*) malloc(sizeof(AST));
+    if (tree == NULL) {
+        HANDLE_ERROR("Memory allocation failed", 99, NULL);
+    }
+    tree->root = NULL;
+    return tree;
+}
 
 int main(int argc, char const *argv[]) {
     return 0;
