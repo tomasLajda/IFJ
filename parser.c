b181c4fd29924abbef1da3a9109246b1359bd3de
Martin Valapka <martin.valapka1@gmail.com>
2024-10-29 16:41:44 +0100

    Added implementation of funcDef parsing

--- parser.c
+++ parser.c
@@ -48,3 +48,57 @@ void parseProlog() {
         HANDLE_ERROR("Expected 'const' in prolog", SYNTAX_ERROR, currentToken);
     }                        
 }
+
+void parseFuncDefs() {
+    while (true) {
+        if (currentToken->type == KEYWORD_PUB) {
+            parseFuncDef();
+
+            if (currentToken->type != KEYWORD_PUB) {
+                break;
+            }
+        } 
+        else {
+            break;
+        }
+    }
+}
+
+void parseFuncDef() {
+    if (currentToken->type == KEYWORD_PUB) {
+        getNextToken(currentToken);
+
+        if (currentToken->type == KEYWORD_FN) {
+            getNextToken(currentToken);
+
+            if (currentToken->type == TOKEN_TYPE_IDENTIFIER) {
+                getNextToken(currentToken);
+
+                if (currentToken->type == TOKEN_TYPE_LEFT_BR) {
+                    getNextToken(currentToken);
+                    parseParams();
+
+                    if (currentToken->type == TOKEN_TYPE_RIGHT_BR) {
+                        getNextToken(currentToken);
+                        parseFunc();
+                    }
+                    else {
+                        HANDLE_ERROR("Expected ')' in function definition", SYNTAX_ERROR, currentToken);
+                    }
+                }
+                else {
+                    HANDLE_ERROR("Expected '(' in function definition", SYNTAX_ERROR, currentToken);
+                }
+            }
+            else {
+                HANDLE_ERROR("Expected function identifier in function definition", SYNTAX_ERROR, currentToken);
+            }
+        }
+        else {
+            HANDLE_ERROR("Expected 'fn' in function definition", SYNTAX_ERROR, currentToken);
+        }
+    }
+    else {
+        HANDLE_ERROR("Expected 'pub' in function definition", SYNTAX_ERROR, currentToken);
+    }
+}
