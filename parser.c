8074667e90237497ecdd10f2cb76017860c8aa3d
Martin Valapka <martin.valapka1@gmail.com>
2024-11-01 23:30:15 +0100

    Added implementation of parseFunc parsing

--- parser.c
+++ parser.c
@@ -13,40 +13,35 @@ SymbolTable* symbolTable = NULL;
 AST* ast = NULL;
 
 void parseProlog() {
-    if (currentToken->type == KEYWORD_CONST) {
-        getNextToken(currentToken);
-
-        if (currentToken->type == KEYWORD_IFJ) {
-            getNextToken(currentToken);
-
-            if (currentToken->type == TOKEN_TYPE_ASSIGN) {
-                getNextToken(currentToken);
-
-                if (currentToken->type == KEYWORD_IMPORT) {
-                    getNextToken(currentToken);
-
-                    if (currentToken->type == TOKEN_TYPE_SEMICOLON) {
-                        getNextToken(currentToken);
-                    }
-                    else {
-                        HANDLE_ERROR("Expected ';' in prolog", SYNTAX_ERROR, currentToken);
-                    } 
-                }
-                else {
-                    HANDLE_ERROR("Expected '@import' in prolog", SYNTAX_ERROR, currentToken);
-                }
-            }
-            else {
-                HANDLE_ERROR("Expected '=' in prolog", SYNTAX_ERROR, currentToken);
-            }
-        }
-        else {
-            HANDLE_ERROR("Expected 'ifj' in prolog", SYNTAX_ERROR, currentToken);
-        }
-    }
-    else {
+    if (currentToken->type != KEYWORD_CONST) {
         HANDLE_ERROR("Expected 'const' in prolog", SYNTAX_ERROR, currentToken);
-    }                        
+        return;
+    }
+    getNextToken(currentToken);
+
+    if (currentToken->type != KEYWORD_IFJ) {
+        HANDLE_ERROR("Expected 'ifj' in prolog", SYNTAX_ERROR, currentToken);
+        return;
+    }
+    getNextToken(currentToken);
+
+    if (currentToken->type != TOKEN_TYPE_ASSIGN) {
+        HANDLE_ERROR("Expected '=' in prolog", SYNTAX_ERROR, currentToken);
+        return;
+    }
+    getNextToken(currentToken);
+
+    if (currentToken->type != KEYWORD_IMPORT) {
+        HANDLE_ERROR("Expected '@import' in prolog", SYNTAX_ERROR, currentToken);
+        return;
+    }
+    getNextToken(currentToken);
+
+    if (currentToken->type != TOKEN_TYPE_SEMICOLON) {
+        HANDLE_ERROR("Expected ';' in prolog", SYNTAX_ERROR, currentToken);
+        return;
+    }
+    getNextToken(currentToken);
 }
 
 void parseFuncDefs() {
@@ -65,40 +60,54 @@ void parseFuncDefs() {
 }
 
 void parseFuncDef() {
-    if (currentToken->type == KEYWORD_PUB) {
-        getNextToken(currentToken);
-
-        if (currentToken->type == KEYWORD_FN) {
-            getNextToken(currentToken);
-
-            if (currentToken->type == TOKEN_TYPE_IDENTIFIER) {
-                getNextToken(currentToken);
-
-                if (currentToken->type == TOKEN_TYPE_LEFT_BR) {
-                    getNextToken(currentToken);
-                    parseParams();
-
-                    if (currentToken->type == TOKEN_TYPE_RIGHT_BR) {
-                        getNextToken(currentToken);
-                        parseFunc();
-                    }
-                    else {
-                        HANDLE_ERROR("Expected ')' in function definition", SYNTAX_ERROR, currentToken);
-                    }
-                }
-                else {
-                    HANDLE_ERROR("Expected '(' in function definition", SYNTAX_ERROR, currentToken);
-                }
-            }
-            else {
-                HANDLE_ERROR("Expected function identifier in function definition", SYNTAX_ERROR, currentToken);
-            }
-        }
-        else {
-            HANDLE_ERROR("Expected 'fn' in function definition", SYNTAX_ERROR, currentToken);
-        }
-    }
-    else {
+    if (currentToken->type != KEYWORD_PUB) {
         HANDLE_ERROR("Expected 'pub' in function definition", SYNTAX_ERROR, currentToken);
+        return;
     }
+    getNextToken(currentToken);
+
+    if (currentToken->type != KEYWORD_FN) {
+        HANDLE_ERROR("Expected 'fn' in function definition", SYNTAX_ERROR, currentToken);
+        return;
+    }
+    getNextToken(currentToken);
+
+    if (currentToken->type != TOKEN_TYPE_IDENTIFIER) {
+        HANDLE_ERROR("Expected function identifier in function definition", SYNTAX_ERROR, currentToken);
+        return;
+    }
+    getNextToken(currentToken);
+
+    if (currentToken->type != TOKEN_TYPE_LEFT_BR) {
+        HANDLE_ERROR("Expected '(' in function definition", SYNTAX_ERROR, currentToken);
+        return;
+    }
+    getNextToken(currentToken);
+    parseParams();
+
+    if (currentToken->type != TOKEN_TYPE_RIGHT_BR) {
+        HANDLE_ERROR("Expected ')' in function definition", SYNTAX_ERROR, currentToken);
+        return;
+    }
+    getNextToken(currentToken);
+    parseFunc();
+}
+
+void parseFunc() {
+    parseType();
+
+    if (currentToken->type != TOKEN_TYPE_LEFT_BR) {
+        HANDLE_ERROR("Expected '{' in function definition", SYNTAX_ERROR, currentToken);
+        return;
+    }
+    getNextToken(currentToken);
+
+    parseStatements();
+    parseReturn();
+
+    if (currentToken->type != TOKEN_TYPE_RIGHT_BR) {
+        HANDLE_ERROR("Expected '}' in function definition", SYNTAX_ERROR, currentToken);
+        return;
+    }
+    getNextToken(currentToken);
 }
