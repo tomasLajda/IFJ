d5ecf23c8a99e6f5720b3495d9da15450c2461f2
VojtaGajda <gajda.vojta@gmail.com>
2024-11-13 11:54:42 +0100

    changed func params

--- code_generator.h
+++ code_generator.h
@@ -16,20 +16,18 @@ IFJ project
  * @brief Generates code from the given abstract syntax tree (AST) and writes it to the specified
  * output file.
  *
- * @param outputFile A pointer to the file where the generated code will be written.
  * @param ast A pointer to the abstract syntax tree representing the source code.
  * @return int Returns 0 on success, or a non-zero error code on failure.
  */
-int generateCode(FILE *outputFile, AST *ast);
+int generateCode(AST *ast);
 
 /**
  * @brief Generates the header for the output file.
  *
  * This function writes the necessary header information to the provided output file.
  *
- * @param outputFile A pointer to the FILE object where the header will be written.
  * @return int Returns 0 on success, or a non-zero error code on failure.
  */
-int generateFileHeader(FILE *outputFile);
+int generateFileHeader();
 
 #endif
