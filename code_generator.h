5f46fb09e863d1b06ec7fa8ce3984fa5c857cc1a
VojtaGajda <gajda.vojta@gmail.com>
2024-11-13 11:19:46 +0100

    added basic structure to header file

--- code_generator.h
+++ code_generator.h
@@ -5,3 +5,13 @@ IFJ project
 
 @author Vojtěch Gajdušek - xgajduv00
 */
+
+#ifndef _CODE_GENERATOR_H
+#define _CODE_GENERATOR_H
+
+#include "ast.h"
+#include <stdio.h>
+
+int generateCode(FILE *outputFile, AST *ast);
+
+#endif
