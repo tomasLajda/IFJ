d47e95d9e78aa930c605ec5c13e2c64da22ab2ed
VojtaGajda <gajda.vojta@gmail.com>
2024-11-13 13:08:27 +0100

    added basic implementation of generating header

--- code_generator.c
+++ code_generator.c
@@ -5,3 +5,35 @@ IFJ project
 
 @author Vojtěch Gajdušek - xgajduv00
 */
+#include "code_generator.h"
+#include "error_codes.h"
+
+DynamicString codeBuffer;
+
+int generateCode(FILE *outputFile, AST *ast) {
+    if (ast == NULL) {
+        return INTERNAL_ERROR;
+    }
+    if (outputFile == NULL) {
+        return INTERNAL_ERROR;
+    }
+
+    dynamicStringInit(&codeBuffer);
+
+    generateCodeHeader();
+
+    codeGeneratorFlush(outputFile);
+
+    return 0;
+}
+
+int generateCodeHeader() {
+    dynamicStringAddString(&codeBuffer, "# Header\n");
+    dynamicStringAddString(&codeBuffer, ".IFJcode24\n");
+    return 0;
+}
+
+void codeGeneratorFlush(FILE *outputFile) {
+    fputs(codeBuffer.string, outputFile);
+    dynamicStringFree(&codeBuffer);
+}
