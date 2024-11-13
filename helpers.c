56e167ae3ad0c97d02b5e6891109bcec9418a446
Matúš Csirik <matus.csirik@gmail.com>
2024-11-13 20:06:38 +0100

    moved helpful copy functions to helpers.h and .c

--- helpers.c
+++ helpers.c
@@ -9,6 +9,7 @@
 
 #include "helpers.h"
 #include "enums.h"
+#include "error_codes.h"
 #include "scanner.h"
 #include <stdio.h>
 
@@ -72,8 +73,6 @@ char *TokenTypeToString(TokenType type) {
         return ";";
     case TOKEN_TYPE_DOT:
         return ".";
-    case TOKEN_TYPE_AT:
-        return "@";
     case TOKEN_TYPE_EXPR:
         return "EXPR";
     case TOKEN_TYPE_DOLLA:
@@ -81,4 +80,87 @@ char *TokenTypeToString(TokenType type) {
     default:
         return "UNKNOWN_TOKEN_TYPE";
     }
+}
+
+Token *createToken(TokenType type) {
+    Token *token = (Token *)malloc(sizeof(Token));
+    if (token == NULL) {
+        HANDLE_ERROR("Memory allocation failure", INTERNAL_ERROR, NULL);
+    }
+    token->type = type;
+    return token;
+}
+
+Token *copyToken(Token *token) {
+    if (token == NULL) {
+        return NULL;
+    }
+    Token *newToken = (Token *)malloc(sizeof(Token));
+    if (newToken == NULL) {
+        HANDLE_ERROR("Memory allocation failure", INTERNAL_ERROR, NULL);
+    }
+    newToken->type = token->type;
+    newToken->line = token->line;
+
+    // Deep copy attribute based on its type
+    switch (token->type) {
+    case TOKEN_TYPE_IDENTIFIER:
+    case TOKEN_TYPE_STRING_VALUE:
+        if (token->attribute.string != NULL) {
+            newToken->attribute.string = (char *)malloc(strlen(token->attribute.string) + 1);
+            if (newToken->attribute.string == NULL) {
+                HANDLE_ERROR("Memory allocation failure", INTERNAL_ERROR, NULL);
+            }
+            strcpy(newToken->attribute.string, token->attribute.string);
+        } else {
+            newToken->attribute.string = NULL;
+        }
+        break;
+    case TOKEN_TYPE_INTEGER_VALUE:
+        newToken->attribute.integer = token->attribute.integer;
+        break;
+    case TOKEN_TYPE_DOUBLE_VALUE:
+        newToken->attribute.decimal = token->attribute.decimal;
+        break;
+    case TOKEN_TYPE_KEYWORD:
+        newToken->attribute.keyword = token->attribute.keyword;
+        break;
+    default:
+        // For token types without attributes
+        break;
+    }
+
+    return newToken;
+}
+
+ASTNode *copyASTNode(ASTNode *node) {
+    if (node == NULL) {
+        return NULL;
+    }
+    ASTNode *newNode = initASTNode();
+    newNode->token = copyToken(node->token);
+    *newNode->parent = *node->parent;
+
+    // Copy left child and set parent
+    if (node->left != NULL) {
+        newNode->left = copyASTNode(node->left);
+    }
+
+    // Copy right child and set parent
+    if (node->right != NULL) {
+        newNode->right = copyASTNode(node->right);
+    }
+
+    return newNode;
+}
+
+StackElement *createStackElement(Token *token, ASTNode *astNodePtr) {
+    StackElement *element = (StackElement *)malloc(sizeof(StackElement));
+    if (element == NULL) {
+        HANDLE_ERROR("Memory allocation failure", INTERNAL_ERROR, NULL);
+    }
+    element->ASTNodePtr = astNodePtr;
+    element->tokenPtr = token;
+    element->next = NULL;
+    return element;
 }
--- helpers.h
+++ helpers.h
@@ -7,11 +7,37 @@
  *
  */
 
+#ifndef HELPERS_H
+
+#include "ast.h"
 #include "dynamic_string.h"
 #include "enums.h"
 #include "scanner.h"
+#include "stack.h"
 
 /**
  * @brief Function that transforms the token type integer to the corresponding character (string)
  */
 char *TokenTypeToString(TokenType type);
+
+/**
+ * @brief Function that creates a new token with the given type
+ */
+Token *createToken(TokenType type);
+
+/**
+ * @brief Function that creates a deep copy of the given token
+ */
+Token *copyToken(Token *token);
+
+/**
+ * @brief Function that creates a deep copy of the given ASTNode
+ */
+ASTNode *copyASTNode(ASTNode *node);
+
+/**
+ * @brief Function that creates a new stack element with the given token and ASTNode
+ */
+StackElement *createStackElement(Token *token, ASTNode *astNodePtr);
+
+#endif // HELPERS_H
