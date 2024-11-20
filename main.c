eea548057f9323fc3077133be4efbde7419e2a1c
VojtaGajda <gajda.vojta@gmail.com>
2024-11-20 23:31:25 +0100

    main c tests

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include "ast.h"
+#include "code_generator.h"
 #include "helpers.h"
 #include <stdio.h>
 
@@ -6,8 +7,14 @@ int main() {
 
     AST *newAST = initAST();
     newAST->root = mockASTProgramStructure(3);
+    // newAST->root->left->left->right = mockASTProgramStructure(4);
+    // newAST->root->left->left->right->left->left = mockASTProgramStructure(2);
+    // newAST->root->left->left->right->left->right = mockASTProgramStructure(9);
     displayAST(newAST);
     // displayAST(newAST->);
+
+    FILE *outputFile = fopen("generated_code.ifjcode", "w");
+    generateCode(outputFile, newAST);
 
     // freeAST(newAST);
     return 0;
