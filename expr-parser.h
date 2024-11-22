387b286171bbf769e37195ae8f37e0126bd2b7bc
Matúš Csirik <matus.csirik@gmail.com>
2024-11-22 03:58:06 +0100

    added newest expr-parser

--- expr-parser.h
+++ expr-parser.h
@@ -16,16 +16,24 @@ IFJ Project
 #include "stack.h"
 
 /**
- * @brief Begins parsing the expression token by token until the end of the expression.
- * Returns a pointer to the AST of the expression through the 'exprAST' parameter.
- * Returns a pointer to the token that ended the expression through the 'token' parameter.
- *  *
- * @param exprAST Pointer to the AST for the expression
- * @param token Pointer to the token that ended the expression (either ';' or ')')
+ * @brief Parses an expression and constructs its Abstract Syntax Tree (AST).
  *
- * @return Returns 0 if the expression was parsed successfully, SYNTAX_ERROR (2) if a syntax error
- * was encountered, INTERNAL_ERROR (99) if an internal parsing error was encountered
+ * This function implements a shift-reduce parsing algorithm to analyze the given
+ * tokens and build the corresponding AST. It utilizes a parsing and input stacks,
+ * reduction rules, and checks the correctness of the syntax of the expression.
+ *
+ * @param exprAST Pointer to the AST structure to be populated.
+ * @param firstToken Pointer to the optional first token of the expression.
+ * @param secondToken Pointer to the optional second token of the expression.
+ * @param delimiterToken Pointer to the token in which to store the delimiter.
+ *
+ * @note firstToken and secondToken are optional and can be NULL. They can be used independently.
+ *
+ * @return
+ *   - 0 on successful parsing.
+ *   - SYNTAX_ERROR (2) if a syntax error is encountered.
+ *   - INTERNAL_ERROR (99) for internal failures (e.g., memory allocation issues).
  */
-int parseExpression(AST *exprAST, Token *token);
+int parseExpression(AST *exprAST, Token *firstToken, Token *secondToken, Token *delimiterToken);
 
 #endif // EXPR_PARSER_H
